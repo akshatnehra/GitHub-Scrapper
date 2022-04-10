f7466ebc6c85d2cc6085b95326afac275fac4452
Akshat Nehra <akshatnehra1999@gmail.com>
2022-04-11 02:03:39 +0530

    v1 pdf functionality added and fixed some minor bugs

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const url = "https://github.com/topics";
 const axios = require('axios');
 const fs = require("fs");
 const pdf = require("pdfkit");
+const path = require("path");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
@@ -15,11 +16,12 @@ axios.get(url)
     const document = dom.window.document;
     let banners = document.querySelectorAll(".no-underline.d-flex.flex-column.flex-justify-center");
     
+    let topicName = document.querySelectorAll(".f3.lh-condensed.text-center.Link--primary.mb-0.mt-1");
+
     for(let i=0; i<banners.length; i++){
         let link = "https://github.com" + banners[i].href;
-        console.log(link);
-
-        openRepo(link);
+        console.log(topicName[i].textContent.trim());
+        openRepo(link, topicName[i].textContent.trim());
     }
   })
   .catch(function (error) {
@@ -27,7 +29,7 @@ axios.get(url)
     console.log(error);
   })
 
-  function openRepo(link){
+  function openRepo(link, topicName){
     axios.get(link)
         .then(function (response) {
             // handle success
@@ -39,7 +41,7 @@ axios.get(url)
 
                 let newLink = "https://github.com" + repos[i].href;
                 // console.log("       "+newLink);
-                openIssues(newLink);
+                openIssues(newLink, topicName);
 
             }
         })
@@ -49,7 +51,7 @@ axios.get(url)
         })
   }
 
-  function openIssues(link){
+  function openIssues(link, topicName){
     axios.get(link)
         .then(function (response) {
             // handle success
@@ -63,7 +65,7 @@ axios.get(url)
             let newLink = "https://github.com" + issue.href;
             // console.log(newLink);
 
-            storeIssues(newLink);            
+            storeIssues(newLink, topicName);            
         })
         .catch(function (error) {
             // handle error
@@ -71,7 +73,7 @@ axios.get(url)
         })
   }
 
-  function storeIssues(link){
+  function storeIssues(link, topicName){
     axios.get(link)
         .then(function (response) {
         // handle success
@@ -84,12 +86,34 @@ axios.get(url)
             let issueDetails = {};
             if(individualIssues[i] == null || individualIssues[i] == undefined)
                 continue;
+            issueDetails.topicName = topicName;
             issueDetails.issueName = individualIssues[i].textContent;
-            issueDetails.issueLink = individualIssues[i].href;
+            issueDetails.issueLink = "https://github.com" + individualIssues[i].href;
             data.push(issueDetails);
         }
 
-        console.table(data);
+        // console.table(data);
+
+        let json = JSON.stringify(data);
+        fs.writeFileSync("data.json", json, "utf-8");
+
+        const doc = new pdf();
+        let outputPath = path.join("OutputFiles",topicName+'.pdf');
+        doc.pipe(fs.createWriteStream(outputPath));
+        // doc.text(json);
+        for(let i=0; i<data.length; i++){
+            if(data[i].topicName == topicName){
+                doc.text(data[i].topicName);
+                // doc.text(data[i].issueName);
+                // doc.text(data[i].issueLink);
+                doc.text(data[i].issueName, {
+                    link: data[i].issueLink,
+                    underline: true
+                  });
+                doc.text("\n");
+            }
+        }
+        doc.end();
     })
     .catch(function (error) {
         // handle error
